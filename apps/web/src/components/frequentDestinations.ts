5815ca51697ef4c3c843de4734da139fbeb80f8e
Hsi <its.hsichen@gmail.com>
2026-06-26 23:38:29 +0800

    chore(repo): move shared config into packages

--- apps/web/src/components/frequentDestinations.ts
+++ apps/web/src/components/frequentDestinations.ts
@@ -1,4 +1,4 @@
-import destinationAutofillConfig from '../../../../lib/destination-autofill/config.json';
+import destinationAutofillConfig from '../../../../packages/config/destination-autofill.json';
 
 const FREQUENT_DESTINATIONS_KEY = 'ontrack_frequent_destinations';
 const LEGACY_RECENT_STATIONS_KEY = 'ontrack_recent_stations';
