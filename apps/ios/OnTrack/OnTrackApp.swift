0db21c97c049be760e1f6e000ee0e85ba1b429bd
Hsi <its.hsichen@gmail.com>
2026-07-22 18:18:57 +0800

    chore(ios): remove widget design artifacts

--- apps/ios/OnTrack/OnTrackApp.swift
+++ apps/ios/OnTrack/OnTrackApp.swift
@@ -5,15 +5,7 @@ import UIKit
 struct OnTrackApp: App {
     var body: some Scene {
         WindowGroup {
-#if DEBUG
-            if ProcessInfo.processInfo.arguments.contains("--widget-preview") {
-                WidgetPreviewHost()
-            } else {
-                appContent
-            }
-#else
             appContent
-#endif
         }
     }
 
@@ -35,19 +27,3 @@ struct OnTrackApp: App {
         UIPasteboard.general.string = message
     }
 }
-
-#if DEBUG
-private struct WidgetPreviewHost: View {
-    var body: some View {
-        ZStack {
-            Color(red: 244 / 255, green: 246 / 255, blue: 249 / 255)
-                .ignoresSafeArea()
-
-            TrainWidgetContent(snapshot: .preview)
-                .frame(width: 338, height: 158)
-                .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
-                .shadow(color: .black.opacity(0.08), radius: 12, y: 6)
-        }
-    }
-}
-#endif
